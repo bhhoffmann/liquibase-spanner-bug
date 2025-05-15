0bfc3b59572991890b2cd00f116de50c66519abc
Bjørn Håvard Hoffmann <bjorn.havard@strise.ai>
2025-05-15 09:40:22 +0200

    ignore target files

--- target/classes/db/test.sql
+++
@@ -1,7 +0,0 @@
--- liquibase formatted sql
-
--- changeset liquibase:1
-CREATE TABLE test_table (
-                            test_id STRING(MAX) NOT NULL,
-                            test_column STRING(MAX) NOT NULL,
-) PRIMARY KEY (test_id);
